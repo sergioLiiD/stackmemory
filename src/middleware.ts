98eeb1a19714c5b73114647e05833e4de6492ed6
Sergio <sergio@hoom.mx>
2025-12-22 10:19:07 +0100

    fix en middleware para problema de auth callback

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,6 +47,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
